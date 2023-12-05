2d0170952395f31813aefc543d9d277d372732e7
Martinpasaribu <83689864+Martinpasaribu@users.noreply.github.com>
2023-12-05 17:17:52 +0700

    V 006

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 // import Typed from 'react-typed';
-
+import logo from '../assets/Logo_main.png';
 
 const Hero = () => {
   return (
     <div className="text-white">
+    
         <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-        <p className='md:text-2xl text-[#05386b] font-bold p-2' > Welcome To My Website</p>
-        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-4'> Mar-Tec</h1>
+        <p className='md:text-xl text-[#05386b] font-bold p-2' > Welcome To My Website</p>
+        
+        <img className='sm:hidden max-w-[150px] mx-auto' src={logo} alt='/' />
+        <h1 className='invisible sm:visible md:text-7xl sm:text-6xl text-4xl font-bold md:py-4'> Mar-Tec</h1>
             <div className='flex justify-center items-center'>
             <p className='md:text-3xl sm:text-4xl text-xl font-bold '> Fast, Flexible Use</p> 
            
